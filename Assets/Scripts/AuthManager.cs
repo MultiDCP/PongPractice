4959f23834b2c5d556cb3d1731d7b239f002defe
SangminPark <psmdc0714@gmail.com>
2022-03-02 18:58:57 +0900

    Update AuthManager.cs
    Add Firebase Initializing and Login Function

--- Assets/Scripts/AuthManager.cs
+++ Assets/Scripts/AuthManager.cs
@@ -21,11 +21,47 @@ public class AuthManager : MonoBehaviour
 
     public void Start()
     {
+        signInButton.interactable = false;
 
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>{
+            var result = task.Result;
+            if(result != DependencyStatus.Available){
+                Debug.LogError(result.ToString());
+                IsFirebaseReady = false;
+            }
+            else{
+                IsFirebaseReady = true;
+
+                firebaseApp = FirebaseApp.DefaultInstance;
+                firebaseAuth = FirebaseAuth.DefaultInstance;
+            }
+
+            signInButton.interactable = IsFirebaseReady;
+        });
     }
 
     public void SignIn()
     {
+        if(!IsFirebaseReady || IsSignInOnProgress || User != null) return;
+        
+        IsSignInOnProgress = true;
+        signInButton.interactable = false;
 
+        firebaseAuth.SignInWithEmailAndPasswordAsync(emailField.text, passwordField.text).ContinueWithOnMainThread(task => {
+            Debug.Log($"Sign in status : {task.Status}");
+
+            IsSignInOnProgress = false;
+            signInButton.interactable = true;
+
+            if(task.IsFaulted)
+                Debug.LogError(task.Exception);
+            else if(task.IsCanceled)
+                Debug.LogError("Sign in canceled");
+            else{
+                User = task.Result;
+                Debug.Log(User.Email);
+                SceneManager.LoadScene("Lobby");
+            }
+        });
     }
 }
